09642d0599a0f73f147e182eb558cee79aabd99d
aice030 <cheng000811@gmail.com>
2025-11-05 22:14:19 +0800

    创建新的java项目aice-juc, 用于juc的学习

---
+++ aice-juc/src/Main.java
@@ -0,0 +1,10 @@
+import alternatePrint.TwoNum;
+
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+        TwoNum tn = new TwoNum();
+        tn.printTwoNum(100);
+    }
+}
