eb37e5a037eec4f7927caf61c070b1547226ba2d
aice030 <cheng000811@gmail.com>
2025-11-12 22:00:02 +0800

    懒汉式单例模式

---
+++ aice-juc/src/Singleton/Singleton.java
@@ -0,0 +1,22 @@
+package Singleton;
+
+/**
+ * 懒汉式单例模式
+ * @author aice
+ */
+public class Singleton {
+    private static volatile Singleton instance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
